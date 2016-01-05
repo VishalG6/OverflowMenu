a288208eda7169eb0d8c5842f122f795afca9f83
VishalG6 <vishalg_94@hotmail.com>
2016-01-05 22:28:30 +1100

    Minor changes

--- app/src/main/java/com/example/vishal/overflowmenu/MainActivity.java
+++ app/src/main/java/com/example/vishal/overflowmenu/MainActivity.java
@@ -1,8 +1,8 @@
 package com.example.vishal.overflowmenu;
 
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.RelativeLayout;
